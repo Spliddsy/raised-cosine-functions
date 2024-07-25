2f7eff50f0c425fe225a82d750d2c475852a4a9a
Jeremiah Rhys Wimer <jeremiahrwimer@gmail.com>
2024-07-25 10:02:50 -0500

    Add files via upload

--- RRCt.m
+++ RRCt.m
@@ -1,5 +1,16 @@
-function result = RRCt(t,alpha,T)
+function result = RRCt(t,alpha,Ts)
+% Generates a VERSION OF the RRC pulse (there are other definitions but
+% this is valid)
 
-R = 1/T;
+tol = 0.0001 * Ts;
 
-result = (sqrt(2.*R) ./ (1 - 64.*alpha.^2.*R.^2.*t.^2)) .* (sin(2.*pi.*R.*(1-alpha).*t)./(2.*pi.*R.*t) + 4.*alpha./pi .* cos(2.*pi.*R.*(1+alpha).*t));
+result = 0 * t;
+R = 0.5/Ts;
+
+cond1 = abs(t) < tol;
+cond2 = abs(abs(t) - (64 * alpha^2 * R^2)^(-1/2)) < tol;
+cond3 = not(cond1 | cond2);
+
+result(cond1) = sqrt(2.*R) .* (1 - alpha + 4.*alpha./pi); % check this is correct
+result(cond2) = (sqrt(2.*R) ./ (2.*pi.*R.*(1 - 192.*(alpha.^2).*(R.^2).*(t(cond2).^2)))).*(2.*pi.*R.*(1-alpha).*cos(2.*pi.*R.*(1-alpha).*t(cond2)) + 8.*R.*alpha.*(cos(2.*pi.*R.*(1+alpha).*t(cond2)) - 2.*pi.*R.*(1+alpha).*t(cond2).*sin(2.*pi.*R.*(1+alpha).*t(cond2))));
+result(cond3) = (sqrt(2.*R) ./ (1 - 64.*alpha.^2.*R.^2.*t(cond3).^2)) .* (sin(2.*pi.*R.*(1-alpha).*t(cond3))./(2.*pi.*R.*t(cond3)) + 4.*alpha./pi .* cos(2.*pi.*R.*(1+alpha).*t(cond3)));
